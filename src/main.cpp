917e814a370dfebbb55c244a88b1ec049248f9a1
Johannes Krainer <j.krainer@posteo.de>
2024-02-20 15:32:24 +0100

    hello world for native windows

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+// #include <esp32_smartdisplay.h>
+
+// void setup()
+// {
+//   smartdisplay_init();
+
+//   auto disp = lv_disp_get_default();
+//   // lv_disp_set_rotation(disp, LV_DISP_ROT_90);
+//   // lv_disp_set_rotation(disp, LV_DISP_ROT_180);
+//   // lv_disp_set_rotation(disp, LV_DISP_ROT_270);
+// }
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World from PlatformIO!\n");
+    return 0;
+}
