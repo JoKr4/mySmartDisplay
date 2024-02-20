cd098f6526024cba641eec93bf64ce1e1745e460
Johannes Krainer <j.krainer@posteo.de>
2024-02-20 21:34:54 +0100

    build for esp32 instead windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
-// #include <esp32_smartdisplay.h>
+#include <esp32_smartdisplay.h>
 
-// void setup()
-// {
-//   smartdisplay_init();
-
-//   auto disp = lv_disp_get_default();
-//   // lv_disp_set_rotation(disp, LV_DISP_ROT_90);
-//   // lv_disp_set_rotation(disp, LV_DISP_ROT_180);
-//   // lv_disp_set_rotation(disp, LV_DISP_ROT_270);
-// }
-
-
-#include <stdio.h>
-
-int main()
+void setup()
 {
-    printf("Hello World from PlatformIO!\n");
-    return 0;
+  smartdisplay_init();
+
+  auto disp = lv_disp_get_default();
+  // lv_disp_set_rotation(disp, LV_DISP_ROT_90);
+  // lv_disp_set_rotation(disp, LV_DISP_ROT_180);
+  // lv_disp_set_rotation(disp, LV_DISP_ROT_270);
 }
+
+void loop()
+{
+  lv_timer_handler();
+}
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("Hello World from PlatformIO!\n");
+//     return 0;
+// }
