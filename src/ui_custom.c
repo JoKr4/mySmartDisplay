9980a6f0c74d5f2df6b002bc49b8a65dcd080707
Johannes Krainer <j.krainer@posteo.de>
2024-03-13 17:25:33 +0100

    optimized layout for 6 buttons

--- src/ui_custom.c
+++ src/ui_custom.c
@@ -8,48 +8,31 @@ void ui_screen_buttons_init(void)
     lv_obj_clear_flag(ui_ScreenButtons, LV_OBJ_FLAG_SCROLLABLE); 
 
     lv_obj_t * cont = lv_obj_create(ui_ScreenButtons);
-    lv_obj_set_size(cont, 300, 240);
+    lv_obj_clear_flag(cont, LV_OBJ_FLAG_SCROLLABLE); 
+    lv_obj_set_size(cont, 300, 320);
     lv_obj_align(cont, LV_ALIGN_TOP_MID, 0, 10);
     lv_obj_set_layout(cont, LV_LAYOUT_GRID);
 
-    static lv_coord_t col_dsc[] = {100, 100, LV_GRID_TEMPLATE_LAST};
-    static lv_coord_t row_dsc[] = {80, 80, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t col_dsc[] = {130, 130, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t row_dsc[] = {90, 90, 90, LV_GRID_TEMPLATE_LAST};
     lv_obj_set_grid_dsc_array(cont, col_dsc, row_dsc);
 
     lv_obj_t * label;
     lv_obj_t * obj;
 
-    obj = lv_btn_create(cont);
-    lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, 0, 1,
-                              LV_GRID_ALIGN_STRETCH, 0, 1);
+    uint32_t i;
+    for(i = 0; i < 9; i++) {
+        uint8_t col = i % 3;
+        uint8_t row = i / 3;
 
-    label = lv_label_create(obj);
-    lv_label_set_text(label, "1");
-    lv_obj_center(label);
+        obj = lv_btn_create(cont);
+        lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, col, 1,
+                                  LV_GRID_ALIGN_STRETCH, row, 1);
 
-    obj = lv_btn_create(cont);
-    lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, 1, 1,
-                              LV_GRID_ALIGN_STRETCH, 0, 1);
-
-    label = lv_label_create(obj);
-    lv_label_set_text(label, "2");
-    lv_obj_center(label);
-
-    // uint32_t i;
-    // for(i = 0; i < 6; i++) {
-    //     uint8_t col = i % 3;
-    //     uint8_t row = i / 3;
-
-    //     obj = lv_btn_create(cont);
-    //     /*Stretch the cell horizontally and vertically too
-    //      *Set span to 1 to make the cell 1 column/row sized*/
-    //     lv_obj_set_grid_cell(obj, LV_GRID_ALIGN_STRETCH, col, 1,
-    //                               LV_GRID_ALIGN_STRETCH, row, 1);
-
-    //     label = lv_label_create(obj);
-    //     lv_label_set_text_fmt(label, "c%d, r%d", col, row);
-    //     lv_obj_center(label);
-    // }
+        label = lv_label_create(obj);
+        lv_label_set_text_fmt(label, "c%d, r%d", col, row);
+        lv_obj_center(label);
+    }
 }
 
 void ui_init(void) {
