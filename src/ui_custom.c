3088284ec833ad3e29ff6c96a30d355a3e4b80db
Johannes Krainer <j.krainer@posteo.de>
2024-03-13 18:09:53 +0100

    added textarea

--- src/ui_custom.c
+++ src/ui_custom.c
@@ -1,19 +1,35 @@
 #include "ui_custom.h"
 
 lv_obj_t * ui_ScreenButtons;
+lv_obj_t * ui_TextArea1;
 
 void ui_screen_buttons_init(void)
 {
     ui_ScreenButtons = lv_obj_create(NULL);
     lv_obj_clear_flag(ui_ScreenButtons, LV_OBJ_FLAG_SCROLLABLE); 
 
-    lv_obj_t * cont = lv_obj_create(ui_ScreenButtons);
-    lv_obj_clear_flag(cont, LV_OBJ_FLAG_SCROLLABLE); 
-    lv_obj_set_size(cont, 300, 320);
-    lv_obj_align(cont, LV_ALIGN_TOP_MID, 0, 10);
+    lv_obj_t * wholeScreen = lv_obj_create(ui_ScreenButtons);
+    lv_obj_clear_flag(wholeScreen, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_set_size(wholeScreen, 320, 480);
+    lv_obj_set_layout(wholeScreen, LV_LAYOUT_GRID);
+    static lv_coord_t col_dsc_1[] = {290, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t row_dsc_1[] = {120, 320, LV_GRID_TEMPLATE_LAST};
+    lv_obj_set_grid_dsc_array(wholeScreen, col_dsc_1, row_dsc_1);
+
+    // text field
+    ui_TextArea1 = lv_textarea_create(wholeScreen);
+    lv_obj_set_grid_cell(ui_TextArea1, LV_GRID_ALIGN_STRETCH, 0, 1,
+                                       LV_GRID_ALIGN_STRETCH, 0, 1);
+    lv_textarea_set_placeholder_text(ui_TextArea1, "Placeholder...");
+
+    // button grid
+    lv_obj_t * cont = lv_obj_create(wholeScreen);
+    lv_obj_set_grid_cell(cont, LV_GRID_ALIGN_STRETCH, 0, 1,
+                               LV_GRID_ALIGN_STRETCH, 1, 1);
+    lv_obj_clear_flag(cont, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_layout(cont, LV_LAYOUT_GRID);
 
-    static lv_coord_t col_dsc[] = {130, 130, LV_GRID_TEMPLATE_LAST};
+    static lv_coord_t col_dsc[] = {125, 125, LV_GRID_TEMPLATE_LAST};
     static lv_coord_t row_dsc[] = {90, 90, 90, LV_GRID_TEMPLATE_LAST};
     lv_obj_set_grid_dsc_array(cont, col_dsc, row_dsc);
 
@@ -33,6 +49,7 @@ void ui_screen_buttons_init(void)
         lv_label_set_text_fmt(label, "c%d, r%d", col, row);
         lv_obj_center(label);
     }
+
 }
 
 void ui_init(void) {
